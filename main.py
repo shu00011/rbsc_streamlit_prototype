82a9e4027bd66b4a050fd2a9e014992441200ab5
shu00011 <koha.koishi.gnu.16@gmail.com>
2023-01-06 21:37:00 +0900

    fix: MULTI機能の追加・要検討要素あり・解決次第mainへマージ

--- main.py
+++ main.py
@@ -31,9 +31,15 @@ def st_print(LISTSIZE, SELECTLIST, A, B, NBINS):
     # histはヒストログラムの値．
     # bin_edgesはビンエッジ．bin_edgesのサイズは常に1+histのサイズ．つまりlength(hist)+1
 
+def dataframe_loc(dataframe, X):
+    return dataframe.loc[X.index].reset_index(drop=True)
+
 def output_df(dataframe,A,B):
-    csvA=dataframe.loc[A.index].reset_index(drop=True).to_csv(index=False)
-    csvB=dataframe.loc[B.index].reset_index(drop=True).to_csv(index=False)
+    dataframeA = dataframe_loc(dataframe,A)
+    dataframeB = dataframe_loc(dataframe,B)
+
+    csvA=dataframeA.to_csv(index=False)
+    csvB=dataframeB.to_csv(index=False)
 
     col0, col1 = st.columns(2)
 
@@ -54,10 +60,21 @@ def output_df(dataframe,A,B):
             mime='text/csv'
         )
 
+def check_userRhostar(userRhostar, name = None):
+    if userRhostar >= -1 and userRhostar <= 1:
+        if name == None:
+            st.info(f'Your RBSC coefficient: {userRhostar}')
+        else:
+            st.info(f'Your RBSC coefficient of {name}: {userRhostar}')
+    else:
+        st.error("⚠ The range of RBSC coefficient must be between -1 and 1.")   
+
 def rbscApp():
    st.title('RBSC-SubGen')
 
    userListsize = 0
+   MAX_SELECT = 2
+   MULTI = False
 
    st.subheader('1. Data upload')
 
@@ -68,14 +85,21 @@ def rbscApp():
 
         st.write(dataframe)
 
-        columns = st.selectbox(
-            'Select the columns you wish to apply to RBSC-SubGen.',
-            (df_columns)
+        columns = st.multiselect(
+            'Select the columns you want to apply to RBSC-SubGen.',
+            (df_columns),
+            max_selections = MAX_SELECT
         )
 
-        read_data = dataframe[columns]
-        userListsize = len(read_data)
+        userListsize=len(dataframe)
         st.info(f'Your number of data points: {userListsize}')
+
+        columns_len = len(columns)
+
+        if columns_len == MAX_SELECT-1 or columns_len == MAX_SELECT:
+            read_data = dataframe[columns[0]]
+            if columns_len == MAX_SELECT:
+                MULTI = True
 
    st.subheader('2. Input parameters')
 
@@ -91,11 +115,15 @@ def rbscApp():
 
    with col1:
         st.write("[RBSC coefficient]")
-        userRhostar=st.number_input('Insert RBSC coefficient')
-        if userRhostar >= -1 and userRhostar <= 1:
-            st.info(f'Your RBSC coefficient: {userRhostar}')
+        
+        if MULTI is not True:
+            userRhostar=st.number_input('Insert RBSC coefficient')
+            check_userRhostar(userRhostar)
         else:
-            st.error("⚠ The range of RBSC coefficient must be between -1 and 1.")
+            userRhostarA = st.number_input(f'Insert RBSC coefficient of: {columns[0]}')
+            check_userRhostar(userRhostarA,columns[0])
+            userRhostarB = st.number_input(f'Insert RBSC coefficient of: {columns[1]}')
+            check_userRhostar(userRhostarB,columns[1])
 
    with col2:
         st.write("[Tolerable error]")
@@ -122,17 +150,63 @@ def rbscApp():
    if st.button('Run'):
         with st.spinner('running...'):
             start_time = time.time()
-            A, B, rho = rbsc.rbsc( \
-                userListsize, \
-                userSelectlist, \
-                userRhostar, \
-                userEps, \
-                read_data, \
-                userMaxtrials)
+            
+            if MULTI is not True:
+                A1, B2, rho = rbsc.rbsc( \
+                    userListsize, \
+                    userSelectlist, \
+                    userRhostar, \
+                    userEps, \
+                    read_data, \
+                    userMaxtrials)
+            else:
+                A, B, rho = rbsc.rbsc( \
+                    userListsize, \
+                    userSelectlist*4, \
+                    userRhostarA, \
+                    userEps, \
+                    read_data, \
+                    userMaxtrials)
 
-            st_print(userListsize, userSelectlist, A, B, userNBins)
+"""
+TODO: このuserSelectlistは2倍にすると，
+2回目（A1とA2を作成するとき）にz-yが0になって上手く処理できない．
+(n=s*2としているため)
 
-            output_df(dataframe,A,B)
+また，同様の理由でuserSelectlistにuserListsizeの4倍の値をユーザが入力して
+しまうと，2回目の処理の時に
+selectlistの要素数とlistsizeの値が等しくなってしまうため，
+RBSC-SubGenアルゴリズムが上手く働かない．
+
+どのくらいに設定すべきか？
+"""
+
+                dataframeA=dataframe_loc(dataframe, A)
+                dataframeB=dataframe_loc(dataframe, B) 
+            
+                read_dataA = dataframeA[columns[1]]
+
+                A1, A2, rho = rbsc.rbsc( \
+                    len(dataframeA), \
+                    userSelectlist, \
+                    userRhostarB, \
+                    userEps, \
+                    read_dataA, \
+                    userMaxtrials)
+                
+                read_dataB = dataframeB[columns[1]]
+
+                B1, B2, rho = rbsc.rbsc( \
+                    len(dataframeB), \
+                    userSelectlist, \
+                    userRhostarB, \
+                    userEps, \
+                    read_dataB, \
+                    userMaxtrials)
+
+            st_print(userListsize, userSelectlist, A1, B2, userNBins)
+
+            output_df(dataframe,A1,B2)
 
             elapsed_time = time.time() - start_time
 
